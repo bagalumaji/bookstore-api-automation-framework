caf826c74ab8c4cdd1c978c469ea6a213c6d1334
umaji bagal <bagalumaji@gmail.com>
2025-09-08 11:13:52 +0530

    created LoginResponse pojo

---
+++ src/main/java/com/bookstore/pojo/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.bookstore.pojo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LoginResponse {
+    private String access_token;
+    private String token_type;
+}
