98eff866447d78255ee0d9724082ec9550bb98b7
Umaji Bagal <bagalumaji@gmail.com>
2025-09-08 16:45:32 +0530

    refactored enum

--- src/main/java/com/bookstore/enums/StatusTypes.java
+++ src/main/java/com/bookstore/enums/StatusTypes.java
@@ -3,16 +3,16 @@ package com.bookstore.enums;
 import lombok.Getter;
 
 @Getter
-public enum SuccessStatusTypes {
+public enum StatusTypes {
     OK(200, "HTTP/1.1 200 OK"),
-    CREATED(201, "CREATED"),
+    BADREQUEST(400, "HTTP/1.1 400 Bad Request"),
     ACCEPTED(202, "ACCEPTED"),
     NO_CONTENT(204, "NO_CONTENT");
 
     private final int code;
     private final String message;
 
-    SuccessStatusTypes(int code, String message) {
+    StatusTypes(int code, String message) {
         this.code = code;
         this.message = message;
     }
