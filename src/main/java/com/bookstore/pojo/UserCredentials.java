d5f17c7fe02a46a11b80f0ec7a794c4e1c2e3c1d
umaji bagal <bagalumaji@gmail.com>
2025-09-08 11:08:50 +0530

    updated with user UserCredentials class

---
+++ src/main/java/com/bookstore/pojo/UserCredentials.java
@@ -0,0 +1,18 @@
+package com.bookstore.pojo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserCredentials {
+    private int id;
+    private String email;
+    private String password;
+}
