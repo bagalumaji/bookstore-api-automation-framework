81a59fcf875e5caca0a88beea6bac76b410ff533
Umaji Bagal <bagalumaji@gmail.com>
2025-09-08 16:44:08 +0530

    added classes for extent report

---
+++ src/main/java/com/bookstore/reports/ExtentManager.java
@@ -0,0 +1,25 @@
+package com.bookstore.reports;
+
+import com.aventstack.extentreports.ExtentTest;
+
+import java.util.Objects;
+
+public final class ExtentManager {
+    private ExtentManager(){}
+
+
+    private static final ThreadLocal<ExtentTest> EXTENT_TEST_THREAD_LOCAL = new ThreadLocal<>();
+
+    public static ExtentTest getExtentTest(){
+        return EXTENT_TEST_THREAD_LOCAL.get();
+    }
+
+    static void setExtentTest(ExtentTest extentTest){
+        if(Objects.nonNull(extentTest)){
+            EXTENT_TEST_THREAD_LOCAL.set(extentTest);
+        }
+    }
+    public static void unload(){
+        EXTENT_TEST_THREAD_LOCAL.remove();
+    }
+}
---
+++ src/main/java/com/bookstore/reports/ExtentReport.java
@@ -0,0 +1,38 @@
+package com.bookstore.reports;
+
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.bookstore.configs.BookstoreConfigReader;
+
+import java.util.Objects;
+
+import static com.bookstore.constants.BookstoreConstants.EXTENT_REPORT_PATH;
+
+public final class ExtentReport {
+    private ExtentReport(){}
+    private static ExtentReports extentReports;
+    public static void initReport(){
+        if(Objects.isNull(extentReports)) {
+            extentReports = new ExtentReports();
+            ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(EXTENT_REPORT_PATH);
+            extentSparkReporter.config().setReportName("Bookstore Api Automation Framework Report");
+            extentSparkReporter.config().setDocumentTitle("Bookstore Api Report");
+            extentSparkReporter.config().setTheme(Theme.DARK);
+            extentReports.attachReporter(extentSparkReporter);
+            extentReports.setSystemInfo("Environment", BookstoreConfigReader.config().environment().toUpperCase());
+            extentReports.setSystemInfo("Automation Engineer","UMAJI BAGAL");
+        }
+    }
+
+    public static void flushReport(){
+        if(Objects.nonNull(extentReports)){
+            extentReports.flush();
+            ExtentManager.unload();
+        }
+    }
+    public static void createTest(String testCaseName){
+        ExtentManager.setExtentTest(extentReports.createTest(testCaseName));
+    }
+}
