fe277c8bea122729bb19edf65b6288e066dc4408
umaji bagal <bagalumaji@gmail.com>
2025-09-08 11:13:20 +0530

    created LoginConstants

---
+++ src/main/java/com/bookstore/constants/LoginConstants.java
@@ -0,0 +1,7 @@
+package com.bookstore.constants;
+
+public final class LoginConstants {
+    public static final String TOKEN_TYPE ="bearer";
+    public static final String LOGIN_WAS_NOT_SUCCESSFUL ="Login was not successful";
+
+}
