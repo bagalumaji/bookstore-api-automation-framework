19bd67d37ec332f5ba5d206ee126a5a598d1f8b1
Umaji Bagal <bagalumaji@gmail.com>
2025-09-08 16:46:14 +0530

    updated SignupApi

--- src/main/java/com/bookstore/apis/signup/SignupApi.java
+++ src/main/java/com/bookstore/apis/signup/SignupApi.java
@@ -1,36 +1,24 @@
 package com.bookstore.apis.signup;
 
+import com.bookstore.client.ApiClient;
 import com.bookstore.configs.BookstoreConfigReader;
 import com.bookstore.pojo.UserCredentials;
-import com.bookstore.specs.ApiRequestSpecs;
 import com.bookstore.specs.ApiResponseSpecs;
 import io.restassured.response.Response;
 
 import static com.bookstore.constants.SignupConstants.MESSAGE;
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public final class SignupApi {
     private SignupApi() {
     }
 
-    public static Response signUpApi(UserCredentials signupRequestData) {
-        return given()
-                .spec(ApiRequestSpecs.getRequestSpec())
-                .body(signupRequestData)
-                .log()
-                .all()
-                .when()
-                .post(BookstoreConfigReader.config().signupEndPoint())
-                .then()
-                .log()
-                .all()
-                .extract()
-                .response();
+    public static Response signUp(UserCredentials signupRequestData) {
+        return ApiClient.post(BookstoreConfigReader.config().signupEndPoint(), signupRequestData);
     }
 
 
-    public static void validateSignApi(Response response) {
+    public static void validateSignup(Response response) {
         response
                 .then()
                 .spec(ApiResponseSpecs.ok200Spec())
