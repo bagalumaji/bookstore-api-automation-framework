062d2ba2e654fde40f4af90efa5c1dc84e1230d5
umaji bagal <bagalumaji@gmail.com>
2025-09-08 11:02:16 +0530

    updated api response specs

---
+++ src/main/java/com/bookstore/specs/ApiResponseSpecs.java
@@ -0,0 +1,64 @@
+package com.bookstore.specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+
+import static com.bookstore.enums.SuccessStatusTypes.OK;
+//todo
+public class ApiResponseSpecs {
+    public static ResponseSpecification ok200Spec() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(OK.getCode())
+                .expectStatusLine(OK.getMessage())
+                .build();
+    }
+
+    public static ResponseSpecification getSuccessResponseSpec() {
+        return new ResponseSpecBuilder()
+                .addResponseSpecification(ok200Spec())
+                .expectStatusCode(HttpStatus.SC_OK)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification getCreatedResponseSpec() {
+        return new ResponseSpecBuilder()
+                .addResponseSpecification(ok200Spec())
+                .expectStatusCode(HttpStatus.SC_CREATED)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification getBadRequestResponseSpec() {
+        return new ResponseSpecBuilder()
+                .addResponseSpecification(ok200Spec())
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification getUnauthorizedResponseSpec() {
+        return new ResponseSpecBuilder()
+                .addResponseSpecification(ok200Spec())
+                .expectStatusCode(HttpStatus.SC_UNAUTHORIZED)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification getNotFoundResponseSpec() {
+        return new ResponseSpecBuilder()
+                .addResponseSpecification(ok200Spec())
+                .expectStatusCode(HttpStatus.SC_NOT_FOUND)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification getNoContentResponseSpec() {
+        return new ResponseSpecBuilder()
+                .addResponseSpecification(ok200Spec())
+                .expectStatusCode(HttpStatus.SC_NO_CONTENT)
+                .build();
+    }
+}
