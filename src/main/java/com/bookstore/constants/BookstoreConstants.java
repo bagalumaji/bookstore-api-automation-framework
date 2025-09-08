cb1024e6d7828333b2cb30f553a530b272efad9b
Umaji Bagal <bagalumaji@gmail.com>
2025-09-08 16:42:54 +0530

    created BookstoreConstants

---
+++ src/main/java/com/bookstore/constants/BookstoreConstants.java
@@ -0,0 +1,7 @@
+package com.bookstore.constants;
+
+public class BookstoreConstants {
+    private BookstoreConstants() {}
+    private static final String BASE_PATH = System.getProperty("user.dir");
+    public static final String EXTENT_REPORT_PATH = BASE_PATH + "/target/extent-reports/index.html";
+}
