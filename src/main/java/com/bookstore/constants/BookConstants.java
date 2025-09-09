96b004508921c0449c1c03a9d0401cc235125519
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 11:26:22 +0530

    created BookConstants

---
+++ src/main/java/com/bookstore/constants/BookConstants.java
@@ -0,0 +1,7 @@
+package com.bookstore.constants;
+
+public final class BookConstants {
+    private BookConstants() {}
+    public static final String BOOK_NOT_FOUND = "Book not found";
+    public static final String BOOK_ID_MISMATCH = "Book ID mismatch";
+}
