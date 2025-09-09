95cf54f566fe902a0bdb16c4d93780d2e45c30c9
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 11:25:14 +0530

    refactored ApiResponseSpecs

--- src/main/java/com/bookstore/specs/ApiResponseSpecs.java
+++ src/main/java/com/bookstore/specs/ApiResponseSpecs.java
@@ -1,22 +1,11 @@
 package com.bookstore.specs;
 
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.ResponseSpecification;
 import org.apache.http.HttpStatus;
 
-import static com.bookstore.enums.StatusTypes.OK;
-//todo
 public class ApiResponseSpecs {
-//    public static ResponseSpecification ok200Spec() {
-//        return new ResponseSpecBuilder()
-//                .expectStatusCode(OK.getCode())
-//                .expectStatusLine(OK.getMessage())
-//                .log(LogDetail.ALL)
-//                .build();
-//    }
-
     public static ResponseSpecification getSuccessResponseSpec() {
         return new ResponseSpecBuilder()
                 .expectStatusCode(HttpStatus.SC_OK)
@@ -24,13 +13,6 @@ public class ApiResponseSpecs {
                 .build();
     }
 
-//    public static ResponseSpecification getCreatedResponseSpec() {
-//        return new ResponseSpecBuilder()
-//                .addResponseSpecification(ok200Spec())
-//                .expectStatusCode(HttpStatus.SC_CREATED)
-//                .expectContentType(ContentType.JSON)
-//                .build();
-//    }
 
     public static ResponseSpecification getBadRequestResponseSpec() {
         return new ResponseSpecBuilder()
@@ -39,12 +21,6 @@ public class ApiResponseSpecs {
                 .build();
     }
 
-    public static ResponseSpecification getUnauthorizedResponseSpec() {
-        return new ResponseSpecBuilder()
-                .expectStatusCode(HttpStatus.SC_UNAUTHORIZED)
-                .expectContentType(ContentType.JSON)
-                .build();
-    }
     public static ResponseSpecification getForbiddenResponseSpec() {
         return new ResponseSpecBuilder()
                 .expectStatusCode(HttpStatus.SC_FORBIDDEN)
@@ -56,12 +32,6 @@ public class ApiResponseSpecs {
         return new ResponseSpecBuilder()
                 .expectStatusCode(HttpStatus.SC_NOT_FOUND)
                 .expectContentType(ContentType.JSON)
-                .build();
-    }
-
-    public static ResponseSpecification getNoContentResponseSpec() {
-        return new ResponseSpecBuilder()
-                .expectStatusCode(HttpStatus.SC_NO_CONTENT)
                 .build();
     }
 }
