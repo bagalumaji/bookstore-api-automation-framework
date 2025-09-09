562ac2a4bbec8a3cada81d3fc44b876fd805f197
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 11:24:59 +0530

    created api constants class

---
+++ src/main/java/com/bookstore/constants/ApiConstants.java
@@ -0,0 +1,10 @@
+package com.bookstore.constants;
+
+public final class ApiConstants {
+    private ApiConstants() {}
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String TOKEN_TYPE ="bearer";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String NOT_AUTHENTICATED = "Not authenticated";
+    public static final String INVALID_TOKEN_OR_EXPIRED_TOKEN = "Invalid token or expired token";
+}
