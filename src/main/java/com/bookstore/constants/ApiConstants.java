25d18d2b5e7536f10d94a5967760f01c9dac640c
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 19:40:07 +0530

    added constants

--- src/main/java/com/bookstore/constants/ApiConstants.java
+++ src/main/java/com/bookstore/constants/ApiConstants.java
@@ -7,4 +7,6 @@ public final class ApiConstants {
     public static final String APPLICATION_JSON = "application/json";
     public static final String NOT_AUTHENTICATED = "Not authenticated";
     public static final String INVALID_TOKEN_OR_EXPIRED_TOKEN = "Invalid token or expired token";
+    public static final String BEARER ="Bearer ";
+    public static final String ID = "id";
 }
