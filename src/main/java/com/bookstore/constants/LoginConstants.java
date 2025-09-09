1d34ab5c122e57b53d19e019de3a5e2ceb9fabae
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 11:37:15 +0530

    added constant

--- src/main/java/com/bookstore/constants/LoginConstants.java
+++ src/main/java/com/bookstore/constants/LoginConstants.java
@@ -2,5 +2,6 @@ package com.bookstore.constants;
 
 public final class LoginConstants {
     public static final String LOGIN_WAS_NOT_SUCCESSFUL ="Login was not successful";
+    public static final String INCORRECT_EMAIL_OR_PASSWORD ="Incorrect email or password";
 
 }
