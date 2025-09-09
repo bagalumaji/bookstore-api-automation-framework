036ed1ae980ba2fce7bb450c652cac0b42deccc1
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 10:44:16 +0530

    added annotation for tests

--- src/test/java/com/bookstore/tests/SignupTests.java
+++ src/test/java/com/bookstore/tests/SignupTests.java
@@ -1,16 +1,15 @@
 package com.bookstore.tests;
 
+import com.bookstore.annotation.Bookstore;
 import com.bookstore.apis.signup.SignupApi;
-import com.bookstore.dataproviders.TestDataProvider;
+import com.bookstore.dataproviders.UserDataProvider;
 import com.bookstore.factory.UserCredentialsFactory;
 import com.bookstore.pojo.UserCredentials;
-import com.bookstore.utils.ValidationUtility;
 import io.restassured.response.Response;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignupTests {
-
+    @Bookstore(author = "Umaji",category = "signup")
     @Test(priority = 1,groups = {"smoke"}, description = "verify Create User Test")
     public void verifyCreateUserTest() {
         // Arrange
@@ -23,7 +22,8 @@ public class SignupTests {
         SignupApi.validateSignup(response);
     }
 
-    @Test(groups = {"smoke"}, priority = 3, dataProvider = "validUsers", dataProviderClass = TestDataProvider.class,description = "Testing signup with valid user data")
+    @Bookstore(author = "Umaji",category = "signup")
+    @Test(groups = {"smoke"}, priority = 2, dataProvider = "validUsers", dataProviderClass = UserDataProvider.class,description = "verifying signup with valid user data Test")
     public void verifySignupForValidUserTest(UserCredentials validUser) {
         Response response = SignupApi.signUp(validUser);
         SignupApi.validateSignup(response);
