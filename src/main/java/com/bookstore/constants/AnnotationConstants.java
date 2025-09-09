7676ac0cd0be2e8dbf785c70204993b9420fe1ef
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 19:39:43 +0530

    created AnnotationConstants

---
+++ src/main/java/com/bookstore/constants/AnnotationConstants.java
@@ -0,0 +1,11 @@
+package com.bookstore.constants;
+
+public final class AnnotationConstants {
+    private AnnotationConstants() {
+    }
+    public static final String UMAJI = "UMAJI";
+    public static final String BOOKS = "BOOKS";
+    public static final String LOGIN = "LOGIN";
+    public static final String SIGNUP = "SIGNUP";
+    public static final String HEALTH = "HEALTH";
+}
