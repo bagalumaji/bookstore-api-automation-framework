720fcd5525361e1f127250779419ebec9ff5e9c0
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 10:40:24 +0530

    refactored methods

--- src/main/java/com/bookstore/apis/login/LoginApi.java
+++ src/main/java/com/bookstore/apis/login/LoginApi.java
@@ -1,9 +1,12 @@
 package com.bookstore.apis.login;
 
 import com.bookstore.client.ApiClient;
+import com.bookstore.constants.LoginConstants;
 import com.bookstore.pojo.UserCredentials;
 import com.bookstore.pojo.LoginResponse;
+import com.bookstore.specs.ApiRequestResponseSpec;
 import io.restassured.response.Response;
+import org.testng.Assert;
 
 import static com.bookstore.configs.BookstoreConfigReader.config;
 
@@ -12,24 +15,18 @@ public class LoginApi {
     public Response login(UserCredentials userCredentials) {
         return ApiClient.post(config().loginEndpoint(), userCredentials);
     }
-
-    public Response login(String username, String password) {
-        UserCredentials userCredentials = UserCredentials.builder()
-                .email(username)
-                .password(password)
-                .build();
-        return login(userCredentials);
+    public Response login(UserCredentials userCredentials, ApiRequestResponseSpec apiRequestResponseSpec) {
+        return ApiClient.post(config().loginEndpoint(), userCredentials, apiRequestResponseSpec);
     }
-
-    public String getAuthToken(String username, String password) {
-        return login(username, password)
-                .as(LoginResponse.class)
-                .getAccess_token();
-    }
-
     public String getAuthToken(UserCredentials userCredentials) {
         return login(userCredentials)
                 .as(LoginResponse.class)
                 .getAccess_token();
+    }
+    public static void validateLoginResponse(LoginResponse loginResponse) {
+        Assert.assertNotNull(loginResponse, "Login response is null");
+        Assert.assertNotNull(loginResponse.getAccess_token(), "Token is null");
+        Assert.assertFalse(loginResponse.getAccess_token().trim().isEmpty(), "Token is empty");
+        Assert.assertEquals(loginResponse.getToken_type(), LoginConstants.TOKEN_TYPE, LoginConstants.LOGIN_WAS_NOT_SUCCESSFUL);
     }
 }
