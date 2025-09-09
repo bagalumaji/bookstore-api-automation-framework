1b4a9d68af71750504fb8f8478c740c8c3219b9f
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 10:40:58 +0530

    created annotation- added Bookstore annotation

---
+++ src/main/java/com/bookstore/annotation/Bookstore.java
@@ -0,0 +1,13 @@
+package com.bookstore.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Bookstore {
+    String author() default "Umaji";
+    String category() default "";
+}
