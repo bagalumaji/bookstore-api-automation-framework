8f3ec90ca680219dbc70dc1a0571e2237fb9a0df
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 10:40:26 +0530

    refactored methods

--- src/main/java/com/bookstore/apis/health/HealthApi.java
+++ src/main/java/com/bookstore/apis/health/HealthApi.java
@@ -4,8 +4,16 @@ import com.bookstore.client.ApiClient;
 import com.bookstore.configs.BookstoreConfigReader;
 import io.restassured.response.Response;
 
+import static com.bookstore.utils.ValidationUtility.*;
+
 public class HealthApi {
     public Response getHealthStatus() {
         return ApiClient.get(BookstoreConfigReader.config().healthEndPoint());
+    }
+    public static void validateHealthResponse(Response response) {
+        validateStatusCode(response, 200);
+        validateResponseTime(response, 5000);
+        validateContentType(response, "application/json");
+        validateResponseNotEmpty(response);
     }
 }
