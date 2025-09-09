00c07e0761b6226bec97d8203091b5757ebaae76
Umaji Bagal <bagalumaji@gmail.com>
2025-09-09 10:39:35 +0530

    added methods in faker

--- src/main/java/com/bookstore/utils/FakerDataUtil.java
+++ src/main/java/com/bookstore/utils/FakerDataUtil.java
@@ -11,4 +11,21 @@ public final class FakerDataUtil {
     public static int getPositiveId() {
         return FakerUtil.getPositiveId();
     }
+
+    public static String getBookTitle() {
+        return FakerUtil.getBookTitle();
+    }
+    public static String getAuthorName() {
+        return FakerUtil.getAuthorName();
+    }
+    public static int getPublishedYear() {
+       return FakerUtil.getPublishedYear();
+    }
+    public static String getBookSummary() {
+       return FakerUtil.getBookSummary();
+    }
+    public static String getBookSummaryAlternative() {
+       return FakerUtil.getBookSummaryAlternative();
+    }
+
 }
--- src/main/java/com/bookstore/utils/FakerUtil.java
+++ src/main/java/com/bookstore/utils/FakerUtil.java
@@ -2,21 +2,64 @@ package com.bookstore.utils;
 
 import net.datafaker.Faker;
 
+import java.util.Random;
+
 public final class FakerUtil {
     private FakerUtil() {
     }
 
+    private static final Random random = new Random();
     private static final Faker faker = new Faker();
+
 
     static String getEmail() {
         return faker.internet().emailAddress();
     }
 
-    static int getPositiveId() {
-        return faker.number().numberBetween(1, 10000);
-    }
-
     static String getPassword() {
         return "Bookstore@123";
+    }
+
+    static String getBookTitle() {
+        return faker.book().title();
+    }
+
+    static String getAuthorName() {
+        return faker.book().author();
+    }
+
+    static int getPublishedYear() {
+        int currentYear = java.time.Year.now().getValue();
+        return faker.number().numberBetween(1900, currentYear + 1);
+    }
+
+    static String getBookSummary() {
+        int sentenceCount = faker.number().numberBetween(3, 6);
+        StringBuilder summary = new StringBuilder();
+
+        for (int i = 0; i < sentenceCount; i++) {
+            summary.append(faker.lorem().sentence());
+            if (i < sentenceCount - 1) {
+                summary.append(" ");
+            }
+        }
+
+        return summary.toString();
+    }
+
+    static String getBookSummaryAlternative() {
+        String[] genres = {"mystery", "romance", "thriller", "fantasy", "science fiction",
+                "historical fiction", "biography", "adventure", "drama"};
+        String genre = genres[random.nextInt(genres.length)];
+
+        return String.format("A captivating %s that follows %s through %s. %s",
+                genre,
+                faker.name().fullName(),
+                faker.lorem().words(5),
+                faker.lorem().sentence(15));
+    }
+
+    static int getPositiveId() {
+        return faker.number().numberBetween(1, 999999);
     }
 }
